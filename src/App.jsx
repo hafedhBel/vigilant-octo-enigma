76d214196afa7e9c02d759ea0b3a844e17b59641
hafedhBel <belhadjali.adam@gmail.com>
2023-12-05 17:13:52 +0100

    adding project to git

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Heading from "./Heading";
+import Food from "./Food";
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Food />
+    </div>
+  );
+}
+
+export default App;
